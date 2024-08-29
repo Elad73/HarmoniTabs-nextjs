d422abf5cbef7379e3b5ef5c11123c9935b65ad1
EladRon <elad.ron.g@gmail.com>
2024-08-29 18:56:19 +0300

    adding logo image and updating header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
 import classes from './header.module.css'
+import Image from "next/image";
+import logoImg from '@/assets/logo.png'
 
 
 export default function Header()
 {
     return (
+        
         <div className={classes.header}>
-        <Link href="/">Home</Link>
-        <Link href="/tabs">All tabs</Link>
-        <Link href="/artists">Tabs by Artists</Link>
-        <Link href="/about">About</Link>
-    </div>
+            <div>
+                <Link className={classes.logo} href="/">
+                    <Image src={logoImg} alt="Harmonica player" priority />HarmoniTabs
+                </Link>
+            </div>
+            <nav className={classes.nav} >
+                <ul>
+                    <li>
+                        <Link href="/tabs">All tabs</Link>
+                    </li>
+                    <li>
+                        <Link href="/artists">Tabs by Artists</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 }
