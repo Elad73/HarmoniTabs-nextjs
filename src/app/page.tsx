7d63580bff8e963464b8b0d1d191e0d51754e757
EladRon <elad.ron.g@gmail.com>
2024-08-30 08:08:10 +0300

    adding homepage image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import homeImg from "public/images/harmonica-band.png"
 
 
 export default function Home() {
-  return <h1>home page</h1>;
+  return (
+    <div>
+      <h1>home page</h1>
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="Harmonica band playing on stage"
+          fill
+          style={{ objectFit: 'cover' }}
+          />
+      </div>
+    </div>
+
+  );
 }
